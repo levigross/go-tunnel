a5260193e73f8ffa9a614b5c85fe18a01ee4ef68
Levi Gross <levi@levigross.com>
2013-02-21 23:42:14 -0500

    Fixed the test script so it works on other systems

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 go build
-./GoTunnel --client &
-./GoTunnel --server --port=1234 --remote-port=9000 &
+BIN_NAME="$(basename `pwd` )"
+./$BIN_NAME --client &
+./$BIN_NAME --server --port=1234 --remote-port=9000 &
 echo "Starting Netcat on port 9000. Open another console and type in nc 127.0.0.1 1212"
 nc -l 9000 
-rm GoTunnel
+rm $BIN_NAME
